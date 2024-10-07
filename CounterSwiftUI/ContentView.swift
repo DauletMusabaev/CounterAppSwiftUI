d9fda5014b1b0851925191107208c609c71d0a37
DauletMusabaev <justicedestrouk@gmail.com>
2024-10-07 14:13:00 +0500

    Реализована логика счётчика

--- CounterSwiftUI/ContentView.swift
+++ CounterSwiftUI/ContentView.swift
@@ -22,9 +22,25 @@ struct ContentView: View {
                 .font(.system(size: 80, weight: .bold))
                 .padding()
                 .foregroundColor(Color.primary) // Поддержка темной темы
+
+            HStack(spacing: 20) {
+                Button(action: incrementCounter) {
+                    Text("+")
+                        .font(.largeTitle)
+                        .frame(width: 80, height: 80)
+                        .background(Color.green) // Цвет останется статическим
+                        .foregroundColor(.white)
+                        .cornerRadius(40)
+                }
+            }
         }
         .padding()
         .background(Color(UIColor.systemBackground)) // Автоматическая поддержка фона
+    }
+
+    // Метод для увеличения значения счетчика
+    private func incrementCounter() {
+        counter += 1
     }
 }
 
