03f38e0133b6f0e0d7bd8a0859a4eb283a5da257
DauletMusabaev <justicedestrouk@gmail.com>
2024-10-07 13:46:39 +0500

    Initial Commit

---
+++ CounterSwiftUI/CounterSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterSwiftUIApp.swift
+//  CounterSwiftUI
+//
+//  Created by Daulet Musabaev on 07.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct CounterSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
