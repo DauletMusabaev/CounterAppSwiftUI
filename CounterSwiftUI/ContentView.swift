ea8c1d899f74ba402e339d4256d4cc69f9c3f51e
DauletMusabaev <justicedestrouk@gmail.com>
2024-10-07 14:11:43 +0500

    Добавлен основной экран

--- CounterSwiftUI/ContentView.swift
+++ CounterSwiftUI/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter: Int = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Text("Счётчик")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding()
+                .foregroundColor(Color.primary) // Поддержка темной темы
+
+            Text("\(counter)")
+                .font(.system(size: 80, weight: .bold))
+                .padding()
+                .foregroundColor(Color.primary) // Поддержка темной темы
         }
         .padding()
+        .background(Color(UIColor.systemBackground)) // Автоматическая поддержка фона
     }
 }
 
